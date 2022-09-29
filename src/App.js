4e6b17592ab2d88219e10d002528b030118c3218
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-09-29 14:42:57 +0600

    added 3 blogs under the card components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
 import Carts from './components/Carts/Carts';
 
 function App() {
   return (
     <div>
       <Carts></Carts>
+      <Blogs></Blogs>
     </div>
   );
 }
---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <div>
+                <h1>How Does React Work</h1>
+                <p>React basically maintains a tree. This tree is able to do efficient diff computations on the nodes.
+
+                    Think of HTML code as a tree. In fact, that is exactly how the browser treats DOM . React allows to effectively re-construct my DOM in JavaScript and push only those changes to the DOM which have actually occurred.</p>
+            </div>
+            <div>
+                <h1>Difference between props and state</h1>
+                <p>
+                    <li>Props: Props are read only.</li>
+                    <li>Props: Props can not be modified.</li>
+                    <li>State: State changes can be asynchronous.</li>
+                    <li>State: State can be modified using <code>this.setState()</code></li>
+                </p>
+            </div>
+            <div>
+                <h1>Use case of useEffect hook</h1>
+                <p>
+                    <li>1. Side Effect Runs After Every Render</li>
+                    <li>2. Side Effect Runs Only Once After Initial Render</li>
+                    <li>4. Side Effect Runs After Props Value Change</li>
+                    <li>5. Side Effect Runs After Props and State Value Change</li>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
